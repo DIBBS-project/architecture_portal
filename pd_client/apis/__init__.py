632499482a0424e85a3c70f99be5ad937843ac3e
Maverick Chardet <chardetm@gmail.com>
2016-07-20 20:05:45 -0500

    Adding the API clients

---
+++ pd_client/apis/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .executions_api import ExecutionsApi
+from .process_instances_api import ProcessInstancesApi
+from .users_api import UsersApi
---
+++ rp_client/apis/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .cluster_definitions_api import ClusterDefinitionsApi
+from .host_definitions_api import HostDefinitionsApi
+from .users_api import UsersApi
